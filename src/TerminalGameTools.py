fc76e758ffbbf8282fc3de7b685b86e505885e6a
Shane McDonough <shanemcdman@gmail.com>
2022-01-07 17:52:08 -0800

    Use single quote docstrings

--- src/TerminalGameTools.py
+++ src/TerminalGameTools.py
@@ -2,21 +2,21 @@ import colorama
 from msvcrt import getch
 
 def move_cursor(x: int, y: int):
-    """
+    '''
     colorama.init must be called for this function to work
     Uses ANSI escape codes to move the cursor to an absolute position.
     :x: the x coordinate to move to. the smallest value that will do anything is 1
     :y: the y coordinate to move to. the smallest value that will do anything is 1
-    """
+    '''
     print(f'\033[{y};{x}H', end='', flush=True)
 
 def move_cursor_rel(x: int, y: int):
-    """
+    '''
     colorama.init must be called for this function to work
     Uses ANSI escape codes to move the cursor to a relative position
     :x: the x coordinate to move relatively
     :y: the y coordinate to move relatively
-    """
+    '''
     if y < 0: # y is less than 0
         print(f'\033[{abs(y)}A', end='', flush=True) # move y down
     elif y > 0: # y is more than 0
@@ -27,7 +27,7 @@ def move_cursor_rel(x: int, y: int):
         print(f'\033[{abs(x)}D', end='', flush=True) # move abs(x) left
 
 def give_options(options: [any, ...], cursor: str = '>', prompt: str = None, starting_index: int = 0) -> (int, any):
-    """
+    '''
     colorama.init must be called for this function to work
     Allows the user to select from a list of options visually using arrow keys/wasd and enter/space
     :options: a list of options that will be displayed
@@ -35,7 +35,7 @@ def give_options(options: [any, ...], cursor: str = '>', prompt: str = None, sta
     :prompt: the prompt to be printed before the the menu
     :starting_index: the index the cursor will start at
     :returns: a tuple containing the index of the selected item and the value
-    """
+    '''
     cursor_len = len(cursor)
     options_len = len(options)
     empty_cursor = ' ' * cursor_len
@@ -70,22 +70,22 @@ def give_options(options: [any, ...], cursor: str = '>', prompt: str = None, sta
     return current_index, options[current_index]
 
 def get_yes_no_response(cursor: str = '>', prompt: str = None) -> bool:
-    """
+    '''
     colorama.init must be called for this function to work
     Allows a user to select yes or no using arrow keys/wasd and enter/space
     :cursor: the cursor that will be printed before the current option
     :prompt: the prompt to be printed before the the menu
     :returns: a boolean; True if yes was selected and False if No was
-    """
+    '''
     return not give_options(['Yes', 'No'], cursor, prompt)
 
 def get_response(prompt: str = None, format_args: (any, ...) = None) -> str:
-    """
+    '''
     Asks user for input then waits for input
     :prompt: The string to print before
     :format_args: the args to be passed to the format statement
     :returns: the string entered by the user
-    """
+    '''
     if prompt is None:
         prompt = 'Please enter {}: '
     if format_args is not None:
